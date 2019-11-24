935f5c951b3906ec89b382ea8c91e17fca8abffc
Vinny <57027830+VinnyTan20@users.noreply.github.com>
2019-11-24 19:06:52 +0700

    update

---
+++ CookieRun/CookieRun/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieRun
+{
+    class Player
+    {
+        int highscore;
+        string namaPlayer;
+        int coin;
+        string []cookie;
+        string []pet;
+
+        public Player()
+        {
+            this.highscore = 0;
+            this.namaPlayer = "";
+            this.Coin = 0;
+        }
+
+
+        public int Coin { get => coin; set => coin = value; }
+        public string[] Cookie { get => cookie; set => cookie = value; }
+        public string[] Pet { get => pet; set => pet = value; }
+
+        public override string ToString()
+        {
+            return namaPlayer + " - " + highscore;
+        }
+    }
+}
---
+++ CookieRun/CookieRun/SaveLoad.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace CookieRun
+{
+    class SaveLoad
+    {
+        Player user;
+        private void Save()
+        {
+            XmlTextWriter writer = new XmlTextWriter("player.xml", Encoding.UTF8);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartElement("root");
+            writer.WriteStartElement("uang");
+            writer.WriteElementString("coin", user.Coin.ToString());
+            writer.WriteEndElement();
+            for (int i = 0; i < user.Cookie.Length; i++)
+            {
+                writer.WriteStartElement("skin");
+                writer.WriteElementString("name", user.Cookie[i]);
+                writer.WriteEndElement();
+            }
+            for (int i = 0; i < user.Pet.Length; i++)
+            {
+                writer.WriteStartElement("pet");
+                writer.WriteElementString("name", user.Pet[i]);
+                writer.WriteEndElement();
+            }
+            writer.WriteEndElement();
+            writer.Close();
+        }
+
+        private void Load()
+        {
+            if(File.Exists("player.xml"))
+            {
+                try
+                {
+                    XmlTextReader reader = new XmlTextReader("player.xml");
+                    reader.ReadStartElement("root");
+                    reader.ReadStartElement("uang");
+                    user.Coin = Convert.ToInt32(reader.ReadElementString("coin"));
+                    reader.ReadEndElement();
+                    int ctr = 0;
+                    while(reader.IsStartElement("skin"))
+                    {
+                        reader.ReadStartElement("skin");
+                        user.Cookie[ctr]= reader.ReadElementString("name");
+                        reader.ReadEndElement();
+                        ctr++;
+                    }
+                    int ctr1 = 0;
+                    while (reader.IsStartElement("pet"))
+                    {
+                        reader.ReadStartElement("pet");
+                        user.Pet[ctr1] = reader.ReadElementString("name");
+                        reader.ReadEndElement();
+                        ctr1++;
+                    }
+                    reader.ReadEndElement();
+                    reader.Close();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            else
+            {
+                
+            }   
+        }
+    }
+}
